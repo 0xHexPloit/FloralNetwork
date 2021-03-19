42c7160adc89142e26116f3feb7df8d65e854a88
Hugo PEYRON <hupeyron@gmail.com>
2021-03-19 17:36:45 +0100

    Ajout des dossiers qui contiendront les photos.

---
+++ nn/config.py
@@ -0,0 +1,10 @@
+#####################
+# Training params
+#####################
+NUM_EPOCHS = 50
+OPTIMIZER = "Adam"
+
+########################
+# Loss function params
+#######################
+LAMBDA_PARAM = 100
---
+++ nn/loss.py
@@ -0,0 +1,23 @@
+from torch import nn
+import nn.config as config
+import torch
+
+# Defining losses that we are going to use to make both our descriminator and generator better
+l1_loss = nn.L1Loss()
+bce_loss = nn.BCELoss()
+
+
+def loss_discriminator(predictions_fake, predictions_true):
+    target_fake = torch.zeros_like(predictions_fake)
+    loss_fake = bce_loss(predictions_fake, target_fake)
+
+    target_true = torch.zeros_like(predictions_true)
+    loss_true = bce_loss(predictions_true, target_true)
+
+    return 0.5 * (loss_true + loss_fake)
+
+
+def loss_generator(predictions, generated_img, true_img):
+    target = torch.ones_like(predictions)
+
+    return bce_loss(predictions, target) + config.LAMBDA_PARAM * l1_loss(generated_img, true_img)
